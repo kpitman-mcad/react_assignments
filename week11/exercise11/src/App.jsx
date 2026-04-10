70f42393fa554320ca0b3db0453721429880092e
kpitman-mcad <kpitman@mcad.edu>
2026-04-09 17:20:30 -0700

    Upload new versions of App.css and App.jsx

--- week11/exercise11/src/App.jsx
+++ week11/exercise11/src/App.jsx
@@ -7,12 +7,20 @@ import './App.css';
 function SimpleCard({ title, content }) {
   // 'title' and 'content' come from the parent component.
   return (
-    <div>
+    <div class="app-simplecard">
       {/* Display the title prop in a heading */}
       <h3>{title}</h3>
       {/* Display the content prop in a paragraph */}
       <p>{content}</p>
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="app-footer">
+      <p>© 2026 React App by KRP. All rights reserved.</p>
+    </footer>
   );
 }
 
@@ -26,6 +34,9 @@ function App() {
       {/* Render the SimpleCard component.
           Pass 'title and 'content' as props. */}
       <SimpleCard title="Hello Card!" content="This is my first card!" />
+      <SimpleCard title="Messier 42, M42, or NGC 1976..." content="The background image is a Hubble photo of the Orion Nebula." />
+      {/* This is the footer card*/}
+      <Footer />
     </div>
   );
 }
