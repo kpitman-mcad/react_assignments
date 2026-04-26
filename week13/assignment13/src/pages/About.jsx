2fc92b3e641e466dcedc294cb1767c6c1f7469b5
kpitman-mcad <kpitman@mcad.edu>
2026-04-26 16:44:58 -0700

    Update About.jsx
    Adding to about section text.

--- week13/assignment13/src/pages/About.jsx
+++ week13/assignment13/src/pages/About.jsx
@@ -3,7 +3,12 @@ function About() {
     <div>
       <h1>About Page</h1>
       <section className="about">
-        This is the about page of our project application.
+        Kevin is currently enrolled in the Master of Arts in Graphic and Web
+        Design program at Minneapolis College of Art and Design. Through this
+        Programming for Web course, he has created and developed numerous
+        animations in the p5.js distribution, as highlighted on this site. This
+        site itself is a demonstration of his skills developing a page using
+        React.
       </section>
     </div>
   );
